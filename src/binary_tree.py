ae5c182596021aae8a720f7721a8e2acc5e7bd54
arturlevickij <74044788+arturlevickij@users.noreply.github.com>
2023-10-20 14:43:10 +0400

    Update binary_tree.py

--- src/binary_tree.py
+++ src/binary_tree.py
@@ -5,13 +5,6 @@ class TreeNode:
         self.right = right
 
 
-root = TreeNode(1)
-root.left = TreeNode(2)
-root.left.left = TreeNode(4)
-root.left.right = TreeNode(5)
-root.right = TreeNode(3)
-
-
 def sum_of_depths(root, depth=0, sum_depth=None):
     if sum_depth is None:
         sum_depth = []
@@ -22,7 +15,3 @@ def sum_of_depths(root, depth=0, sum_depth=None):
         if root.right:
             sum_of_depths(root.right, depth + 1, sum_depth)
     return sum(sum_depth)
-
-
-
-print(sum_of_depths(root))
